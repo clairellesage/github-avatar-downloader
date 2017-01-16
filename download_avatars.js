ce78434df8238f26077fa25dcedc2c68df16be3e
clairellesage <clairellesage@gmail.com>
2017-01-16 23:28:39 +0000

    cb added!

--- download_avatars.js
+++ download_avatars.js
@@ -15,42 +15,37 @@ var GITHUB_TOKEN = "43205e5bfcb6012e0c87ac41ab9585ebca5b08d0";
 var repoOwner = "clairellesage"
 var repoName = "github-avatar-downloader"
 
+function getAvatar(data) {
+	for (i in data) {
+	var avatar = data[i].avatar_url;
+	console.log(avatar)
+	}
+}
 
 function getRepoContributors(repoOwner, repoName, cb) {
-	var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-	return requestURL;		
-	}
+	var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';	
 
-  request.get(options, function(error, response, body) {
-    // If we have an error, we need to deal with it.
-    if (error) {
-      // console.log("Boom! Deal with it!");
-      // console.log(error);
-      return error;
-    }
+	request.get(options, function(error, response, body) {
+	  // If we have an error, we need to deal with it.
+	  if (error) {
+	    // console.log("Boom! Deal with it!");
+	    // console.log(error);
+	    return error;
+	  }
 
-    if (response) {                        
-        console.log('Response Status Code: ', response.statusCode);
-    }
+	  if (response) {                        
+	      console.log('Response Status Code: ', response.statusCode);
+	  }
 
-    if (response.statusCode === 200) {
+	  if (response.statusCode === 200) {
 					var data = JSON.parse(body);
-					console.log("This is the Response body:", data);
-					for (i in data) {
-						var avatar = data[i].avatar_url;
-						console.log(avatar)
-					}
-
-					console.log("Found avatar url:", avatar);
-					// cb(avatar);
+					cb(data)
 		}
 
 
 
-      })
+    })
 
+}
 
-
-
-
-getRepoContributors("jquery", "jquery")
+getRepoContributors("jquery", "jquery", getAvatar)
