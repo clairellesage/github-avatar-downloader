1ad375f4ff66f59d93208bdfe8e0f0a5dc6d7c32
clairellesage <clairellesage@gmail.com>
2017-01-17 01:39:55 +0000

    last commit

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,5 @@
+// Alex Wilmer put this here because request was bombing out
+var fetch = require('isomorphic-fetch')
 var request = require('request');
 var fs = require('fs');
 
@@ -14,14 +16,17 @@ var GITHUB_USER = "clairellesage";
 var GITHUB_TOKEN = "43205e5bfcb6012e0c87ac41ab9585ebca5b08d0";
 
 function downloadImageByURL(url, filePath) {
+
+	console.log('!!!', url)
+
 	if (!fs.existsSync("avatar")) {
 		fs.mkdirSync('./avatar')
 	}
  	request.get(url)
-       .pipe(fs.createWriteStream("./avatar/" + filePath));
+       .pipe(fs.createWriteStream(filePath));
 
 
-}
+}	
 
 function listAvatarURL(data){
  for (i= 0; i< data.length; i++) {
@@ -38,22 +43,28 @@ var options = {
    }
  };
 
-   request.get(options, function(error, response, body) {
-     if (error) {
-     console.log(error);
-     return error;
-   }
-     console.log(response.statusCode);
-
-     if (response.statusCode === 200) {
-       var data = JSON.parse(body);
-       cb(data)
+   fetch(options.url, options.headers).then(r => r.json()).then(data => {
+   	  cb(data)
        // for (i=0; i<data.length; i++)
        // console.log(data[i].avatar_url)
-   }
-
-
    })
+
+   // request.get(options, function(error, response, body) {
+   //   if (error) {
+   //   console.log(error);
+   //   return error;
+   // }
+   //   console.log(response.statusCode);
+
+   //   if (response.statusCode === 200) {
+   //     var data = JSON.parse(body);
+   //     cb(data)
+   //     // for (i=0; i<data.length; i++)
+   //     // console.log(data[i].avatar_url)
+   // 	}
+
+
+   // })
 
 
 }
