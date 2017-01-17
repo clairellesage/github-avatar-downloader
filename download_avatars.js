1692e4f274f902079893c09fc22bc09ac0e3dc7f
clairellesage <clairellesage@gmail.com>
2017-01-17 02:00:40 +0000

    final commit

--- download_avatars.js
+++ download_avatars.js
@@ -3,11 +3,14 @@ var fetch = require('isomorphic-fetch')
 var request = require('request');
 var fs = require('fs');
 
+var argowner = process.argv[2];
+var argrepo = process.argv[3];
+
 var options = {
- url: 'https://api.github.com/repos/jquery/jquery/contributors',
+  url: 'https://api.github.com/repos/jquery/jquery/contributors',
   headers: {
     'User-Agent': 'clairellesage'
-}
+	}
 };
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -17,37 +20,31 @@ var GITHUB_TOKEN = "43205e5bfcb6012e0c87ac41ab9585ebca5b08d0";
 
 function downloadImageByURL(url, filePath) {
 
-	console.log('!!!', url)
-
 	if (!fs.existsSync("avatar")) {
-		fs.mkdirSync('./avatar')
+    fs.mkdirSync('./avatar')
 	}
  	request.get(url)
-       .pipe(fs.createWriteStream(filePath));
-
-
+         .pipe(fs.createWriteStream(filePath));
 }	
 
-function listAvatarURL(data){
+function listAvatarURL(data) {
  for (i= 0; i< data.length; i++) {
    downloadImageByURL(data[i].avatar_url, "avatar/" + data[i].login + ".jpg")
- }
+ 	}
 }
 
 function getRepoContributors(repoOwner, repoName, cb) {
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-var options = {
- url: requestURL,
-  headers: {
-    'User-Agent': 'clairellesage'
-   }
- };
-
-   fetch(options.url, options.headers).then(r => r.json()).then(data => {
+  if (argrepo === undefined || process.argv.length > 4) {
+	 throw "need two arguments!"
+  } else {
+      var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+      fetch(options.url, options.headers).then(r => r.json()).then(data => {
    	  cb(data)
        // for (i=0; i<data.length; i++)
        // console.log(data[i].avatar_url)
    })
+
+   //getting error here, but this is what I would do if request was working for me
 
    // request.get(options, function(error, response, body) {
    //   if (error) {
@@ -62,11 +59,8 @@ var options = {
    //     // for (i=0; i<data.length; i++)
    //     // console.log(data[i].avatar_url)
    // 	}
-
-
    // })
-
-
+  }
 }
 
-getRepoContributors("jquery","jquery", listAvatarURL)
+getRepoContributors(argowner, argrepo, listAvatarURL)
